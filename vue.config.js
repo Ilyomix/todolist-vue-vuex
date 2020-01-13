7c47ef56d9b83c599ec1dd9659482a94ed815f61
Abd-Lillah-EXT <uic57569@contiwan.com>
2020-01-13 15:13:03 +0100

    Adding vuetify

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "configureWebpack": {
+    "resolve": {
+      "alias": {
+        "styles": "C:\\Users\\uic57569\\Work\\vue-todolist\\src\\assets\\styles"
+      }
+    }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
